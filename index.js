7be3e38860111e14cabb8ce6d6fc88de94dd9b61
Bas van der Hoek <bas@hoek.com>
2024-12-30 14:41:40 +0100

    Extract loudness json to a function

--- index.js
+++ index.js
@@ -2,26 +2,30 @@ const {exec} = require('child_process');
 
 const file = process.argv[2]
 
-exec(`ffmpeg -i "${file}" -hide_banner -loglevel info -nostats -filter:a loudnorm=print_format=json -f null /dev/null`, (err, stdout, stderr) => {
-  if (err) {
-    console.error(err)
-    return;
-  }
+function getLoudnessJson(file) {
+  exec(`ffmpeg -i "${file}" -hide_banner -loglevel info -nostats -filter:a loudnorm=print_format=json -f null /dev/null`, (err, stdout, stderr) => {
+    if (err) {
+      console.error(err)
+      return;
+    }
 
-  let capturing = false;
-  let capture = []
-  stderr.split(/\r?\n/).forEach(line => {
-    if (capturing) {
-      capture.push(line);
-    }
-    if (line.startsWith('[Parsed_loudnorm_0')) {
-      capturing = true;
-    }
-    if (line === '}') {
-      capturing = false;
-    }
+    let capturing = false;
+    let capture = []
+    stderr.split(/\r?\n/).forEach(line => {
+      if (capturing) {
+        capture.push(line);
+      }
+      if (line.startsWith('[Parsed_loudnorm_0')) {
+        capturing = true;
+      }
+      if (line === '}') {
+        capturing = false;
+      }
+    });
+
+    let loudnorm = JSON.parse(capture.join('\n'));
+    console.log(loudnorm);
   });
+}
 
-  let loudnorm = JSON.parse(capture.join('\n'));
-  console.log(loudnorm);
-});
+getLoudnessJson(file);
